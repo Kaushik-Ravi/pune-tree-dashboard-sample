c37bf85c0485fb97f2134c85cd32db6caacff1ad
Kaushik Ravi <gautamravi2002@gmail.com>
2025-05-31 16:42:21 +0530

    Configure for GitHub Pages deployment and add build output to docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pune-tree-dashboard-sample/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: { // Add this build object
+    outDir: 'docs' // Specify the output directory as 'docs'
+  }
 });
