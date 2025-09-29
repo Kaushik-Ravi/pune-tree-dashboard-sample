81cff7c60865c21f96ad1aa8afa778e40ce8ffc9
Kaushik Ravi <gautamravi2002@gmail.com>
2025-09-29 19:19:19 +0530

    fix: Remove base path from Vite config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pune-tree-dashboard-sample/',
+  //base: '/pune-tree-dashboard-sample/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
